c70aeb66111eca47ee35d7ed2556cde6b7ec340a
Kevin Stewart <kev2000.com@googlemail.com>
2019-03-25 20:28:16 +0700

    initial commit, creating user accounts, password hashing, and basic logging in implemented, placeholder landing page created to

---
+++ app.py
@@ -0,0 +1,53 @@
+import pymongo
+from datetime import datetime
+from flask import Flask, redirect, render_template, request, session, url_for
+from passlib.hash  import sha256_crypt
+
+mongo_uri = "mongodb+srv://kev:22c2c119f3@cluster0-nnrmm.mongodb.net/bartendr?retryWrites=true"
+DBS_NAME =  "bartendr"
+
+
+app = Flask(__name__)
+
+def mongo_connect(uri):
+    try:
+        conn = pymongo.MongoClient(uri)
+        print("DB Connected Successfully")
+        return conn["bartendr"]
+    except pymongo.error.ConnectionFailure as e:
+        print("Could not connect to MongoDB: %s") % e
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login/', methods=["GET", "POST"])
+def login():
+    connection = mongo_connect(mongo_uri)
+    userCollection = connection["users"]
+    userdetails = userCollection.find_one({"username": request.form["username"]}) 
+    if userdetails != None:
+        if sha256_crypt.verify(request.form["password"], userdetails["passhash"]):
+            return "Success"
+    return "Fail"
+
+
+#Routes for Creating New Content 
+@app.route('/c/new_account', methods=["GET","POST"])
+def new_user():
+    """ Add a New User to the database and Hashes their password """
+    hash = sha256_crypt.hash(request.form["newpassword1"])
+    connection = mongo_connect(mongo_uri)
+    userCollection = connection["users"]
+    userCollection.insert_one(
+        {
+            "username": request.form["newusername"], 
+            "passhash": hash,
+            "date_joined":str(datetime.now())
+        }
+    )
+    print("new user added")
+    return redirect(url_for("index"))
+
+app.run(debug=True)
---
+++ readme.md
@@ -0,0 +1,36 @@
+# bartendr.db
+
+Searching (anybody):
+
+* As a registered or unregistered user I should be able to search for popular cocktails.
+
+* As a registered or unregistered user i should be able to search for cocktails based on ingredients.
+
+* As a registered or unregistered user i should be able to search for cocktails based on flavour.
+
+* As a registered or unregistered user i should be able view a random cocktail.
+
+* As a registered or unregistered user I should be able to see the most recently added cocktails.
+
+* As a registered or unregistered user I should be able to search for a user and see all the cocktails they have submitted.
+
+* As a registered or unregistered user i should be able to view more information about the cocktails
+    * I should be able to look up a glossary of any cocktail mixing terms that i don't understand.
+
+
+Submitting Cocktails (Registered users):
+
+* As a bartendr user I should be able to submit a new cocktails to the database.
+    * As the createor of a cocktail I should be able to edit the recipe.
+    * As the creator of a cocktail I should be able to remove it from the database.
+
+* As a bartendr user when viewing a cocktail I should be able to suggest variations of that cocktail.
+
+* As a bartendr user I should be able to upvote or downvote cocktails that I have enjoyed or not.
+
+* As a bartender user I should be able to flag any cocktails that are innapropriate - offensive, dangerous, trollinng.
+
+Social:
+* As a bartendr user i should be able to leave a comment on peoples cocktails
+
+* AS a bartender user I shoul
