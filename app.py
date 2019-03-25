77e2149aef47f8c0b23366d6f116c27be8643bb7
Kevin Stewart <kev2000.com@googlemail.com>
2019-03-25 21:14:28 +0700

    added sessions to login/logout functionality

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ from datetime import datetime
 from flask import Flask, redirect, render_template, request, session, url_for
 from passlib.hash  import sha256_crypt
 
-mongo_uri = "mongodb+srv://kev:22c2c119f3@cluster0-nnrmm.mongodb.net/bartendr?retryWrites=true"
-DBS_NAME =  "bartendr"
-
 
 app = Flask(__name__)
+#environment variables hide irl
+mongo_uri = "mongodb+srv://kev:22c2c119f3@cluster0-nnrmm.mongodb.net/bartendr?retryWrites=true"
+DBS_NAME =  "bartendr"
+app.secret_key = 'any random string'
+
 
 def mongo_connect(uri):
     try:
@@ -22,6 +24,8 @@ def mongo_connect(uri):
 def index():
     return render_template('index.html')
 
+
+#Login and Logout
 @app.route('/login/', methods=["GET", "POST"])
 def login():
     connection = mongo_connect(mongo_uri)
@@ -29,8 +33,14 @@ def login():
     userdetails = userCollection.find_one({"username": request.form["username"]}) 
     if userdetails != None:
         if sha256_crypt.verify(request.form["password"], userdetails["passhash"]):
-            return "Success"
-    return "Fail"
+            session['username'] = request.form["username"]
+            return redirect(url_for("index"))
+
+@app.route('/logout/')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
 
 
 #Routes for Creating New Content 
@@ -48,6 +58,8 @@ def new_user():
         }
     )
     print("new user added")
+    session['username'] = request.form["newusername"]
     return redirect(url_for("index"))
+
 
 app.run(debug=True)
