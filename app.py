f4f0db4078009a47e8d5807e03b9f9f3c3645697
Kevin Stewart <kev2000.com@googlemail.com>
2019-04-08 22:58:14 +0700

    fixing deployment issues

--- app.py
+++ app.py
@@ -329,8 +329,5 @@ def like_dislike():
         
 
 if __name__ == '__main__':
-    app.run(
-        host=os.environ['IP'],
-        port=int(os.environ['PORT']),
-        debug=True
-    )
+    port = int(os.environ.get("PORT", 33507))
+    app.run(host='0.0.0.0', port=port)  
