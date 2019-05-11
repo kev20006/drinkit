ad71236c908d07309c08b3d057967c51dd5ee203
Kevin Stewart <kev2000.com@googlemail.com>
2019-05-11 14:21:36 +0700

    refactored inline js into files to DRY up the app, added in the search Front-End

---
+++ static/scripts/randomiseAngles.js
@@ -0,0 +1,12 @@
+//script to randomise the rotation of block text page elements
+
+[...document.querySelectorAll(".pink")].forEach(element => {
+    element.style.transform = `rotate(${Math.floor(Math.random() * (6)) - 3}deg)`
+});
+[...document.querySelectorAll(".blue")].forEach(element => {
+    element.style.transform = `rotate(${Math.floor(Math.random() * (6)) - 3}deg)`
+});
+
+[...document.querySelectorAll(".timestamp")].forEach(time => {
+    time.innerText = formatDate(new Date(time.innerText))
+});
